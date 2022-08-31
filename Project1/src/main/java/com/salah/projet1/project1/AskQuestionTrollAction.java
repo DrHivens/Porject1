7a969938a53270f0023cb6d17f2101fbaaaf1e8c
Hivens <salahraquib@gmail.com>
2022-08-31 17:22:40 -0400

    added a class and configured plugin.xml

---
+++ Project1/src/main/java/com/salah/projet1/project1/AskQuestionTrollAction.java
@@ -0,0 +1,14 @@
+package com.salah.projet1.project1;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class AskQuestionTrollAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent e){
+        BrowserUtil.browse("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); //rick roll
+    }
+
+
+}
